629a5801fdf9afa4e76cfb588256f94561b2d27b
LAPTOP-U1KJS04O\gourav <chakraborty69178@gmail.com>
2018-05-01 18:04:32 +0530

    Final Commit

---
+++ Color Switch Replica/Assets/Scripts/Rotator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed = 100f;
+    int[] speeds = { 130,-130};
+    public bool BigCircle=false;
+    public Transform cam;
+    public float offset;
+    public bool notRotating = false;
+    public Rotator small;
+    public GameObject colorChanger;
+    private void Start()
+    {
+        speed = speeds[Random.Range(0, 2)];
+        if (BigCircle)
+        {
+            if (small == null)
+            {
+                Debug.Log("Null in Small");
+                //speed = small.speed * -1;
+            }
+            else {
+                speed = (-small.speed);
+            }
+            
+        }
+    }
+    void Update () {
+        if(this.transform.position.y<(cam.transform.position.y+offset))
+        {
+            Spawner.currentYposition += Spawner.diffrence;
+            Spawner.colorchangeYposition = Spawner.currentYposition + Spawner.diffrence/2;
+            this.transform.position = new Vector2(this.transform.position.x, Spawner.currentYposition);
+            if(colorChanger != null)
+                Instantiate(colorChanger, new Vector2(0, Spawner.colorchangeYposition), Quaternion.identity);
+        }
+        if(!notRotating)
+		transform.Rotate(0f, 0f, speed * Time.deltaTime);
+	}
+}
---
+++ Color Switch Replica/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour {
+
+    //public GameObject[] objects;
+   //public static GameObject colorChanger;
+    public static float colorchangeYposition;
+    public static float currentYposition = 16.33f;
+    public static float diffrence;
+    public static int Score;
+    public Text text;
+	void Start () {
+        currentYposition = 16.33f;
+        diffrence = 7.2f;
+        Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = Score.ToString();
+	}
+}
